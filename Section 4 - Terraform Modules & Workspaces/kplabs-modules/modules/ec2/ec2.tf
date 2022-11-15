392f9f3d50e46f3bcd1d4fbc4f67d3cd572f5045
Tenable.cs <admin@accurics.com>
2022-11-15 14:50:45 +0000

    Auto Remediation by Tenable.cs, file: Section 4 - Terraform Modules & Workspaces/kplabs-modules/modules/ec2/ec2.tf

--- Section 4 - Terraform Modules & Workspaces/kplabs-modules/modules/ec2/ec2.tf
+++ Section 4 - Terraform Modules & Workspaces/kplabs-modules/modules/ec2/ec2.tf
@@ -1,4 +1,33 @@
 resource "aws_instance" "myec2" {
-   ami = "ami-082b5a644766e0e6f"
-   instance_type = var.instance_type
+  ami           = "ami-082b5a644766e0e6f"
+  instance_type = var.instance_type
+}
+
+resource "aws_vpc" "<resource_name>" {
+  cidr_block = "<cidr>"
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_vpc" "<resource_name>" {
+  cidr_block = "<cidr>"
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_vpc" "<resource_name>" {
+  cidr_block = "<cidr>"
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_vpc" "<resource_name>" {
+  cidr_block = "<cidr>"
+
+  tags = {
+    Name = "main"
+  }
 }
