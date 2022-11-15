b6131ab1af851241f4eecfdacde4cd379c35fca3
Tenable.cs <admin@accurics.com>
2022-11-15 14:50:46 +0000

    Auto Remediation by Tenable.cs, file: Section 2 - Read, Generate, Modify Congiruations/fetch-values-variables.tf

--- Section 2 - Read, Generate, Modify Congiruations/fetch-values-variables.tf
+++ Section 2 - Read, Generate, Modify Congiruations/fetch-values-variables.tf
@@ -5,20 +5,49 @@ provider "aws" {
 }
 
 resource "aws_instance" "myec2" {
-   ami = "ami-082b5a644766e0e6f"
-   instance_type = var.list[1]
+  ami           = "ami-082b5a644766e0e6f"
+  instance_type = var.list[1]
 }
 
 variable "list" {
-  type = list
-  default = ["m5.large","m5.xlarge","t2.medium"]
+  type    = list
+  default = ["m5.large", "m5.xlarge", "t2.medium"]
 }
 
 variable "types" {
   type = map
   default = {
-    us-east-1 = "t2.micro"
-    us-west-2 = "t2.nano"
+    us-east-1  = "t2.micro"
+    us-west-2  = "t2.nano"
     ap-south-1 = "t2.small"
+  }
+}
+
+resource "aws_vpc" "<resource_name>" {
+  cidr_block = "<cidr>"
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_vpc" "<resource_name>" {
+  cidr_block = "<cidr>"
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_vpc" "<resource_name>" {
+  cidr_block = "<cidr>"
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_vpc" "<resource_name>" {
+  cidr_block = "<cidr>"
+
+  tags = {
+    Name = "main"
   }
 }
